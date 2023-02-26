d8de9609c866caf5dff5cd4d8bf34ceaca7f7b44
DenisMigin-qa <123251047+DenisMigin-qa@users.noreply.github.com>
2023-02-26 21:37:25 +0300

    Add files via upload

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,58 @@
+describe('Форма логина и пароля', function () {
+
+    it('правильный логин и пароль', function () {
+       cy.visit('https://login.qa.studio/');
+       cy.get('#mail').type('german@dolnikov.ru');
+       cy.get('#pass').type('iLoveqastudio1');
+       cy.get('#loginButton').click();
+       cy.contains('Авторизация прошла успешно').should('be.visible');
+       cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('восстановление пароля', function () {
+        cy.reload();
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#forgotEmailButton').click()
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('правильный логин и НЕправильный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1111');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('НЕправильный логин и правильный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german111@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('логин без @', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('привидение к строчным буквам', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+})
---
+++ cypress/e2e/qastudio_me.cy.js
@@ -0,0 +1,31 @@
+describe('Тестирование сайта testqastudio.me', function () {
+
+    it('тест', function () {
+       cy.visit('https://testqastudio.me/');
+       cy.get('.post-11342 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click();
+       cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase').click();
+       cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase').click();
+       cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click();
+       cy.wait(5000);
+       cy.get('#cart-modal > .off-modal-layer').click();
+       cy.get('.header-left-items > .site-branding > .logo > .logo-dark').click();
+       cy.get('.post-11337 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click();
+       cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click();
+       cy.wait(5000);
+       cy.get('.checkout').click();
+       cy.get('#billing_first_name').type('Denis');
+       cy.get('#billing_last_name').type('Mig');
+       cy.get('#billing_address_1').type('18 Lenin.st');
+       cy.get('#billing_city').type('SPb');
+       cy.get('#billing_state').type('SPb');
+       cy.get('#billing_postcode').type('123213');
+       cy.get('#billing_phone').type('+79999999999');
+       cy.get('#billing_email').type('Denis@mi.com');
+       cy.get('#place_order').click();
+       cy.wait(5000);
+       cy.contains('Ваш заказ принят. Благодарим вас.');
+
+
+
+    })
+})    
